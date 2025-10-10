82f20a78e810edb970c0018154a5dea034039ce1
M-Twin4US <huameizhu02@gmail.com>
2025-10-10 12:19:56 +0100

    Update gh_output_to_hdf5.py

--- gh_output_to_hdf5.py
+++ gh_output_to_hdf5.py
@@ -4,7 +4,7 @@ import h5py as h5
 
 
 for i in range(0, 11):
-#In this case, a total of 10 slices are retrieved from the digital model in Rhino through the Grasshopper script.
+#In this case, a total of 11 slices are retrieved from the digital model in Rhino through the Grasshopper script.
     df = pd.read_csv(r'\gh_output\%s.csv'%i)
     t = np.array(df).astype(int)
     t = np.squeeze(t)
